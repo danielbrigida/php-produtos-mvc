70bb43a162b682505dfb97071135f2c2a832bf85
Daniel Brigida <daniel.brigida@tmax.com.br>
2022-12-31 17:01:59 -0300

    Iniciando estrutura de API

---
+++ app/modules/Api/src/Controller/ApiController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Api\src\Controller;
+
+use App\Core\src\Controller\Controller;
+
+abstract class ApiController extends Controller
+{
+    public function api()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        switch ($method) {
+            case 'GET' :
+                $this->index();
+            break;
+
+            case 'POST' :
+                $this->create();
+            break;
+
+            case 'PUT' :
+                $this->update();
+            break;
+
+            case 'DELETE' :
+                $this->delete();
+            break;
+        }   
+    }
+
+    public function jsonResponse(array $data, $code = 200) {
+        header("Content-Type: application/json", true, $code);
+        echo json_encode($data);
+        die();
+    }
+
+    protected function jsonError(array $data, $code = 404) {
+        header("Content-Type: application/json", true, $code);
+        echo json_encode(['error' => $data]);
+        die();
+    }
+
+    protected abstract function index();
+    protected abstract function update();
+    protected abstract function delete();
+    protected abstract function create();
+}
